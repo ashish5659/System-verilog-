453093288d83b522ad9dde896b8a0b677a35d542
Ashish Ranjan <81768209+ashish5659@users.noreply.github.com>
2024-06-14 14:46:01 +0530

    Create call_by_reference.v

---
+++ oops/call_by_reference.v
@@ -0,0 +1,20 @@
+module tb;
+  task automatic swap (ref bit[1:0]a, bit[1:0]b);
+    bit[1:0] temp;
+    
+    temp=a;
+    a=b;
+    b=temp;
+    $display("swapped value of a:%0d and b:%0d",a,b);
+  endtask
+  
+  bit[1:0] a;
+  bit[1:0] b;
+  initial begin
+    a=1;
+    b=2;
+    swap(a,b);
+    $display("swapped value of a:%0d and b:%0d",a,b);
+  end 
+  
+endmodule
